2dc3c7ea15897dbfe312f05959f08100d96ba447
yangjinpeng <yy1209gogo@163.com>
2021-06-25 16:21:56 +0800

    feat:增加XiUU组件库

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routers'
-
+import XiUU from 'xiuu-ui/packages/index'
+import 'xiuu-ui/packages/theme-default/lib/index.css'
 Vue.config.productionTip = false
+Vue.use(XiUU)
 
 new Vue({
   el: '#app',
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>登录</div>
+  <div>
+    <p>home</p>
+    <div style="display:flex;flex-direction:row;justify-content:center;align-items:flex-start;flex-wrap:wrap;">
+    <xi-button><span slot="title">默认按钮</span></xi-button>
+    <xi-button type="primary">主要按钮</xi-button>
+    <xi-button type="success">成功按钮</xi-button>
+    <xi-button type="info">信息按钮</xi-button>
+    <xi-button type="warning">警告按钮</xi-button>
+    <xi-button type="danger">危险按钮</xi-button>
+  </div>
+  </div>
 </template>
 
 <script>
